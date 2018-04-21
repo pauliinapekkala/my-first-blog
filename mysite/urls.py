31e938fa6344436a4c928ba92cd481141e843198
pauliinapekkala <pauliinapek@gmail.com>
2018-04-21 15:37:17 +0300

    Created hompage and about

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from blog import views
+
 urlpatterns = [
+    url(r'^$', views.home, name="home"),
+    url(r'^about/$', views.about, name="about"),
     url(r'^admin/', admin.site.urls),
 ]
